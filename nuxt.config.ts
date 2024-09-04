713ab377f68f5ef61f2026b0b9e96b071715c194
Catherine Luse <gennitdev@gmail.com>
2024-09-04 15:55:57 -0700

    Clean up dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,32 +10,30 @@ export default defineNuxtConfig({
       },
     },
   },
-  auth: {
-    isEnabled: true,
-    disableServerSideAuth: false,
-    originEnvKey: config.baseUrl,
-    baseURL: "http://localhost:3000/api/auth",
-    provider: {
-      type: "authjs",
-      trustHost: false,
-      defaultProvider: "github",
-      addDefaultCallbackUrl: true,
-      // domain: config.domain,
-      // clientId: config.clientId,
-      // clientSecret: config.clientSecret,
-    },
-    sessionRefresh: {
-      enablePeriodically: true,
-      enableOnWindowFocus: true,
-    },
-  },
+  // auth: {
+  //   isEnabled: true,
+  //   disableServerSideAuth: false,
+  //   originEnvKey: config.baseUrl,
+  //   baseURL: "http://localhost:3000/api/auth",
+  //   provider: {
+  //     type: "authjs",
+  //     trustHost: false,
+  //     defaultProvider: "github",
+  //     addDefaultCallbackUrl: true,
+  //     // domain: config.domain,
+  //     // clientId: config.clientId,
+  //     // clientSecret: config.clientSecret,
+  //   },
+  //   sessionRefresh: {
+  //     enablePeriodically: true,
+  //     enableOnWindowFocus: true,
+  //   },
+  // },
   compatibilityDate: "2024-04-03",
   components: true,
   css: [
     "@/assets/css/index.css",
     "@fortawesome/fontawesome-free/css/all.css",
-    "codemirror/lib/codemirror.css",
-    "codemirror/addon/scroll/simplescrollbars.css",
   ],
   devtools: { enabled: true },
   // fontawesome: {
@@ -69,10 +67,7 @@ export default defineNuxtConfig({
   },
   modules: [
     "@nuxtjs/apollo",
-    "@sidebase/nuxt-auth",
     "@nuxtjs/eslint-module",
-    // "@nuxtjs/fontawesome",
-    // "@nuxtjs/vuetify",
   ],
   plugins: [
     { src: "@/plugins/sentry", mode: "client" },
