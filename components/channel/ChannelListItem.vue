aed9ba338cd3f6ebdcd2bcca6dcf2f56a7dcc3b0
Catherine Luse <gennitdev@gmail.com>
2024-09-28 12:48:08 -0700

    Channel list is more compact

--- components/channel/ChannelListItem.vue
+++ components/channel/ChannelListItem.vue
@@ -26,38 +26,35 @@ const props = defineProps({
 
 const tags = computed(() => props.channel.Tags.map((tag: TagData) => tag.text));
 
-defineEmits(['filterByTag']);
-
+defineEmits(["filterByTag"]);
 </script>
 
 <template>
   <div>
     <div
-      class="border py-4 md:px-6 border-gray-500 dark:border-gray-600 shadow md:rounded-t-lg bg-white p-3 dark:bg-gray-800 dark:text-gray-200"
+      class="border py-2 md:px-4 border-gray-500 dark:border-gray-600 shadow md:rounded-t-lg bg-white p-2 dark:bg-gray-800 dark:text-gray-200"
     >
       <div class="flex flex-row">
         <nuxt-link
           :to="`/forums/${channel.uniqueName}/discussions`"
           class="flex cursor-pointer"
         >
-          <div class="h-24 w-24">
-            <AvatarComponent
-              :text="channel.uniqueName"
-              :src="channel?.channelIconURL || ''"
-              :is-medium="true"
-              :square="true"
-            />
-          </div>
+          <AvatarComponent
+            :text="channel.uniqueName"
+            :src="channel?.channelIconURL || ''"
+            :is-medium="true"
+            :square="true"
+          />
         </nuxt-link>
 
-        <div class="flex flex-col px-4">
+        <div class="flex flex-col px-2">
           <nuxt-link
             :to="`/forums/${channel.uniqueName}/discussions`"
-            class="mt-1 flex cursor-pointer items-center gap-4"
+            class="mt-1 flex cursor-pointer items-center gap-2"
           >
             <h3
               v-if="channel.uniqueName && !channel?.displayName"
-              class="mb-2 mt-4 flex border-gray-700 text-2xl font-bold leading-6 text-gray-500 dark:text-gray-200"
+              class="mb-1 mt-2 text-xl font-bold text-gray-500 dark:text-gray-200"
             >
               <HighlightedSearchTerms
                 :text="channel.uniqueName"
@@ -66,7 +63,7 @@ defineEmits(['filterByTag']);
             </h3>
             <div v-if="channel?.displayName">
               <h3
-                class="mb-2 flex border-gray-700 text-2xl font-bold leading-6 text-gray-500 dark:text-gray-200"
+                class="mb-1 text-xl font-bold text-gray-500 dark:text-gray-200"
               >
                 <HighlightedSearchTerms
                   :text="channel.displayName"
@@ -74,7 +71,7 @@ defineEmits(['filterByTag']);
                 />
               </h3>
               <span
-                class="text-sm font-bold font-mono leading-6 text-gray-500 dark:text-gray-300"
+                class="text-xs font-mono font-bold text-gray-500 dark:text-gray-300"
               >
                 <HighlightedSearchTerms
                   :text="channel.uniqueName"
@@ -83,10 +80,11 @@ defineEmits(['filterByTag']);
               </span>
             </div>
           </nuxt-link>
+
           <div>
             <div
               v-if="channel.description"
-              class="my-1 text-sm flex-wrap font-normal text-gray-600 dark:text-gray-200"
+              class="my-1 text-xs text-gray-600 dark:text-gray-200"
             >
               <HighlightedSearchTerms
                 :text="channel.description"
@@ -94,7 +92,7 @@ defineEmits(['filterByTag']);
               />
             </div>
 
-            <div class="flex gap-2">
+            <div class="flex gap-1">
               <Tag
                 v-for="tag in tags"
                 :key="tag"
@@ -103,18 +101,19 @@ defineEmits(['filterByTag']);
                 @click="$emit('filterByTag', tag)"
               />
             </div>
-          </div>   
+          </div>
         </div>
       </div>
     </div>
+
     <div class="md:rounded-b-lg bg-gray-500 text-white">
-      <div class="flex w-full py-2 px-2">
-        <div class="truncate text-xs font-normal">
+      <div class="flex w-full py-1 px-1">
+        <div class="truncate text-xs">
           <nuxt-link
-            class="flex items-center gap-1 rounded-lg px-4 py-2 hover:bg-gray-700"
+            class="flex items-center gap-1 rounded-lg px-2 py-1 hover:bg-gray-700"
             :to="`/forums/${channel.uniqueName}/discussions`"
           >
-            <DiscussionIcon class="h-4 w-4" />
+            <DiscussionIcon class="h-3 w-3" />
             {{ channel?.DiscussionChannelsAggregate?.count }}
             {{
               channel?.DiscussionChannelsAggregate?.count === 1
@@ -125,13 +124,13 @@ defineEmits(['filterByTag']);
         </div>
         <div
           v-if="channel?.EventChannelsAggregate?.count"
-          class="truncate text-xs font-normal"
+          class="truncate text-xs"
         >
           <nuxt-link
-            class="flex items-center gap-1 rounded-lg px-4 py-2 hover:bg-gray-700"
+            class="flex items-center gap-1 rounded-lg px-2 py-1 hover:bg-gray-700"
             :to="`/forums/${channel.uniqueName}/events/search`"
           >
-            <CalendarIcon class="h-4 w-4" />
+            <CalendarIcon class="h-3 w-3" />
             {{ channel?.EventChannelsAggregate?.count || 0 }} Upcoming Events
           </nuxt-link>
         </div>
