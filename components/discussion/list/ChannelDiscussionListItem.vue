a862078563cb7ab099a541ce9ca0764b4b07fb0b
Catherine Luse <gennitdev@gmail.com>
2024-10-09 14:57:37 -0700

    Rearrange channel discussion list item

--- components/discussion/list/ChannelDiscussionListItem.vue
+++ components/discussion/list/ChannelDiscussionListItem.vue
@@ -105,106 +105,104 @@ const filteredQuery = computed(() => {
 </script>
 
 <template>
-  <li
-    class="m-2 pt-5 flex md:rounded-lg border-gray-800 dark:bg-gray-800"
-  >
+  <li class="m-2 pt-5 flex md:rounded-lg border-gray-800 dark:bg-gray-800">
     <div class="flex w-full flex-row justify-start gap-4 rounded-lg">
       <div v-if="discussion" class="w-full flex-col">
-        <div class="flex gap-1">
-          <div
-            class="mr-2 flex h-10 w-10 justify-center items-center rounded-md bg-gray-100 p-1 text-xl dark:bg-gray-600"
-          >
-            💬
+        <div class="flex gap-4">
+          <div class="flex flex-col gap-2">
+            <DiscussionVotes
+              v-if="discussionChannel"
+              :discussion="discussion"
+              :discussion-channel="discussionChannel"
+              :show-downvote="false"
+            />
           </div>
           <div class="w-full flex-col">
-            <nuxt-link
-              :to="{
-                name: 'forums-forumId-discussions-discussionId',
-                params: {
-                  forumId: defaultUniqueName,
-                  discussionId: discussionChannel.discussionId,
-                },
-                query: filteredQuery,
-              }"
-              class="w-full"
-            >
-              <span
-                class="cursor-pointer hover:text-gray-500 dark:text-gray-100"
+            <div>
+              <nuxt-link
+                :to="{
+                  name: 'forums-forumId-discussions-discussionId',
+                  params: {
+                    forumId: defaultUniqueName,
+                    discussionId: discussionChannel.discussionId,
+                  },
+                  query: filteredQuery,
+                }"
+                class="w-full"
               >
-                <HighlightedSearchTerms
-                  :text="title"
-                  :search-input="searchInput"
-                  :classes="'font-medium text-base hover:underline dark:text-gray-100 dark:hover:text-gray-300'"
+                <span
+                  class="cursor-pointer hover:text-gray-500 dark:text-gray-100"
+                >
+                  <HighlightedSearchTerms
+                    :text="title"
+                    :search-input="searchInput"
+                    :classes="'font-medium text-base hover:underline dark:text-gray-100 dark:hover:text-gray-300'"
+                  />
+                </span>
+              </nuxt-link>
+              <div
+                class="font-medium text-xs text-gray-600 no-underline dark:text-gray-300"
+              >
+                <span class="mr-1 text-xs text-gray-500">{{
+                  `Posted ${relativeTimeAgo} by`
+                }}</span>
+                <UsernameWithTooltip
+                  v-if="authorUsername"
+                  :is-admin="authorIsAdmin"
+                  :is-mod="authorIsMod"
+                  :username="authorUsername"
+                  :src="authorProfilePicURL ?? ''"
+                  :display-name="authorDisplayName ?? ''"
+                  :comment-karma="authorCommentKarma"
+                  :discussion-karma="authorDiscussionKarma"
+                  :account-created="authorAccountCreated"
                 />
-              </span>
-            </nuxt-link>
+              </div>
+            </div>
+
             <div
-              class="font-medium text-xs text-gray-600 no-underline dark:text-gray-300"
+              v-if="discussion?.body"
+              class="my-2 dark:bg-gray-700 bg-gray-100 rounded"
             >
-              <span class="mr-1 text-xs text-gray-500">{{
-                `Posted ${relativeTimeAgo} by`
-              }}</span>
-              <UsernameWithTooltip
-                v-if="authorUsername"
-                :is-admin="authorIsAdmin"
-                :is-mod="authorIsMod"
-                :username="authorUsername"
-                :src="authorProfilePicURL ?? ''"
-                :display-name="authorDisplayName ?? ''"
-                :comment-karma="authorCommentKarma"
-                :discussion-karma="authorDiscussionKarma"
-                :account-created="authorAccountCreated"
+              <MarkdownPreview
+                :text="discussion.body"
+                :disable-gallery="false"
+                :word-limit="50"
+                class="-ml-2"
               />
             </div>
-          </div>
-        </div>
-        <div
-          v-if="discussion?.body"
-          class="my-2 dark:bg-gray-700 bg-gray-100 rounded"
-        >
-          <MarkdownPreview
-            :text="discussion.body"
-            :disable-gallery="false"
-            :word-limit="50"
-            class="-ml-2"
-          />
-        </div>
-        <div
-          class="font-medium my-1 flex space-x-1 text-xs text-gray-600 hover:no-underline"
-        >
-          <TagComponent
-            v-for="tag in tags"
-            :key="tag"
-            class="my-1"
-            :active="selectedTags.includes(tag)"
-            :tag="tag"
-            @click="$emit('filterByTag', tag)"
-          />
-        </div>
-        <div class="flex items-center">
-          <DiscussionVotes
-            v-if="discussionChannel"
-            :discussion="discussion"
-            :discussion-channel="discussionChannel"
-            :show-downvote="false"
-          />
-          <div class="flex items-center justify-start gap-6">
-            <nuxt-link
-              :to="{
-                name: 'forums-forumId-discussions-discussionId',
-                params: {
-                  forumId: defaultUniqueName,
-                  discussionId: discussionChannel.discussionId,
-                },
-                query: filteredQuery,
-              }"
-              class="rounded-md p-1 px-4 underline"
+            <div
+              class="font-medium my-1 flex space-x-1 text-xs text-gray-600 hover:no-underline"
             >
-              <i class="fa-regular fa-comment h-5 w-5" />
-              <span class="text-md">{{
-                `${commentCount} comment${commentCount === 1 ? "" : "s"}`
-              }}</span>
-            </nuxt-link>
+              <TagComponent
+                v-for="tag in tags"
+                :key="tag"
+                class="my-1"
+                :active="selectedTags.includes(tag)"
+                :tag="tag"
+                @click="$emit('filterByTag', tag)"
+              />
+            </div>
+            <div class="flex items-center">
+              <div class="flex items-center justify-start gap-6">
+                <nuxt-link
+                  :to="{
+                    name: 'forums-forumId-discussions-discussionId',
+                    params: {
+                      forumId: defaultUniqueName,
+                      discussionId: discussionChannel.discussionId,
+                    },
+                    query: filteredQuery,
+                  }"
+                  class="rounded-md p-1 pr-4 underline"
+                >
+                  <i class="fa-regular fa-comment h-5 w-5" />
+                  <span class="text-md">{{
+                    `${commentCount} comment${commentCount === 1 ? "" : "s"}`
+                  }}</span>
+                </nuxt-link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
