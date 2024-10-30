9e6f03ef92a6d9c9482c945a9757163a121e5d6a
Catherine Luse <gennitdev@gmail.com>
2024-10-29 20:44:55 -0700

    Change alias config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   alias: {
-    "@": '.',
+    "@": fileURLToPath(new URL('./', import.meta.url)),
   },
   build: {
     transpile: ["vuetify"],
