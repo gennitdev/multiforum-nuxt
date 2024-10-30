8369fbb2ca4d14900ce3391e5a14f51635f0d6b3
Catherine Luse <gennitdev@gmail.com>
2024-10-29 21:41:17 -0700

    Specify node version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,6 @@ import { defineNuxtConfig } from "nuxt/config";
 import config from "./config";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 import path from "path";
-import fs from "fs";
-
-// Log the directory contents of components/nav
-console.log('Nav folder contents:', fs.readdirSync(path.resolve(__dirname, 'components/nav')));
-
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
