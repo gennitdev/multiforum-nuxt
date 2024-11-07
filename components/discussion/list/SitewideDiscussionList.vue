52719c7902f88324da30f5906b1e04d8e92f839b
Catherine Luse <gennitdev@gmail.com>
2024-11-07 01:09:05 -0700

    Remove negative margin

--- components/discussion/list/SitewideDiscussionList.vue
+++ components/discussion/list/SitewideDiscussionList.vue
@@ -176,7 +176,7 @@ const filterByChannel = (channel: string) => {
     <div v-if="discussions && discussions.length > 0" class="p-0">
       <div
         role="list"
-        class="flex flex-col -ml-4 divide-y divide-gray-200 dark:divide-gray-700 bg-white dark:bg-gray-800 shadow sm:rounded-lg"
+        class="flex flex-col divide-y divide-gray-200 dark:divide-gray-700 bg-white dark:bg-gray-800 shadow sm:rounded-lg"
         data-testid="sitewide-discussion-list"
       >
         <SitewideDiscussionListItem
