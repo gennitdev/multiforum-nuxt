7b3d6830082e34a041358801b02a8c0e2d035ad8
Catherine Luse <gennitdev@gmail.com>
2024-11-20 16:07:34 -0700

    Throw error if graphql API call does not work properly

--- cypress/e2e/utils.ts
+++ cypress/e2e/utils.ts
@@ -1,22 +1,52 @@
 export const deleteAll = () => {
-  cy.loginAsAdmin();
+  cy.loginAsAdmin(); // Log in first
 
-  cy.deleteEvents();
-  cy.deleteEmails();
-  cy.deleteUsers();
-  cy.deleteChannels();
-  cy.deleteTags();
-  cy.deleteDiscussions();
-  cy.deleteComments();
-  cy.deleteEventChannels();
-  cy.deleteDiscussionChannels();
+  // Chain all deletions and ensure they throw errors if any fail
+  cy.deleteEvents()
+    .then((response) => validateResponse(response, "deleteEvents"))
+    .then(() => cy.deleteEmails())
+    .then((response) => validateResponse(response, "deleteEmails"))
+    .then(() => cy.deleteUsers())
+    .then((response) => validateResponse(response, "deleteUsers"))
+    .then(() => cy.deleteChannels())
+    .then((response) => validateResponse(response, "deleteChannels"))
+    .then(() => cy.deleteTags())
+    .then((response) => validateResponse(response, "deleteTags"))
+    .then(() => cy.deleteDiscussions())
+    .then((response) => validateResponse(response, "deleteDiscussions"))
+    .then(() => cy.deleteComments())
+    .then((response) => validateResponse(response, "deleteComments"))
+    .then(() => cy.deleteEventChannels())
+    .then((response) => validateResponse(response, "deleteEventChannels"))
+    .then(() => cy.deleteDiscussionChannels())
+    .then((response) => validateResponse(response, "deleteDiscussionChannels"))
+    .then(() => cy.deleteChannelRoles())
+    .then((response) => validateResponse(response, "deleteChannelRoles"))
+    .then(() => cy.deleteModChannelRoles())
+    .then((response) => validateResponse(response, "deleteModChannelRoles"))
+    .then(() => cy.deleteServerRoles())
+    .then((response) => validateResponse(response, "deleteServerRoles"))
+    .then(() => cy.deleteModServerRoles())
+    .then((response) => validateResponse(response, "deleteModServerRoles"))
+    .then(() => cy.deleteServerConfigs())
+    .then((response) => validateResponse(response, "deleteServerConfigs"));
+};
 
-  // RBAC
-  cy.deleteChannelRoles();
-  cy.deleteModChannelRoles();
-  cy.deleteServerRoles();
-  cy.deleteModServerRoles();
-  cy.deleteServerConfigs();
+// Helper function to validate the response
+const validateResponse = (response, commandName) => {
+  // Log the entire response for debugging purposes
+  cy.log(`Response for ${commandName}: ${JSON.stringify(response)}`);
+
+  if (!response || response.status >= 400 || (response.body && response.body.errors)) {
+    const errorDetails = response.body?.errors?.map((error) => {
+      return `Message: ${error.message}, Path: ${JSON.stringify(error.path)}, Extensions: ${JSON.stringify(error.extensions)}`;
+    }).join("\n") || "No detailed error available";
+
+    cy.log(`Error in ${commandName}: ${errorDetails}`);
+    throw new Error(`${commandName} failed with errors: ${errorDetails}`);
+  }
+
+  cy.log(`${commandName} succeeded`);
 };
 
 export const seedAll = () => {
