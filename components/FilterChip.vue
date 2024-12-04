07ea3e122df027833bf2efa3288ac346afd35367
Catherine Luse <gennitdev@gmail.com>
2024-12-04 14:41:31 -0700

    Filter buttons are visible in SSR mode

--- components/FilterChip.vue
+++ components/FilterChip.vue
@@ -55,32 +55,31 @@ export default defineComponent({
 </script>
 
 <template>
-  <client-only>
-    <div class="flex align-items">
-      <Popper
-        :close-on-content-click="false"
-        location="bottom"
-        v-model="isOpen"
+  <div class="flex align-items">
+    <Popper
+      :close-on-content-click="false"
+      location="bottom"
+      v-model="isOpen"
+    >
+      <button
+        :data-testid="dataTestid"
+        :class="[highlighted ? 'border-blue-500 ring-1 ring-blue-500' : '']"
+        class="max-height-3 font-small mr-2 inline-flex whitespace-nowrap rounded-lg bg-white px-3 py-2.5 text-xs text-gray-700 border hover:bg-gray-200 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-600"
       >
-        <button
-          :data-testid="dataTestid"
-          :class="[highlighted ? 'border-blue-500 ring-1 ring-blue-500' : '']"
-          class="max-height-3 font-small mr-2 inline-flex whitespace-nowrap rounded-lg bg-white px-3 py-2.5 text-xs text-gray-700 border hover:bg-gray-200 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-600"
-        >
-          <slot name="icon" />
-
-          {{ label }}
-          <ChevronDownIcon
-            class="-mr-1 ml-1 mt-0.5 h-3 w-3"
-            aria-hidden="true"
-          />
-        </button>
-        <template #content>
+        <slot name="icon" />
+        {{ label }}
+        <ChevronDownIcon
+          class="-mr-1 ml-1 mt-0.5 h-3 w-3"
+          aria-hidden="true"
+        />
+      </button>
+      <template #content>
+        <client-only>
           <div v-if="isOpen" class="rounded-lg border bg-white dark:bg-gray-700">
             <slot name="content" />
           </div>
-        </template>
-      </Popper>
-    </div>
-  </client-only>
+        </client-only>
+      </template>
+    </Popper>
+  </div>
 </template>
