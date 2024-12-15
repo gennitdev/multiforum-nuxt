f2a74ccbc0b545f4b7519568a2e19942a32b5eb5
Catherine Luse <gennitdev@gmail.com>
2024-12-15 12:45:56 -0700

    Hide mobile tabs at desktop width

--- pages/forums/[forumId].vue
+++ pages/forums/[forumId].vue
@@ -11,9 +11,11 @@ import ChannelSidebar from "@/components/channel/ChannelSidebar.vue";
 import { useRoute, useRouter } from "nuxt/app";
 import { useQuery } from "@vue/apollo-composable";
 import { DateTime } from "luxon";
+import { useDisplay } from "vuetify";
 
 const route = useRoute();
 const router = useRouter();
+const { smAndDown } = useDisplay();
 
 const showDiscussionTitle = computed(() =>
   route.name?.toString().includes("forums-forumId-discussions-discussionId")
@@ -139,6 +141,7 @@ if (!channelId.value) {
           class="relative h-full max-w-screen-2xl w-full rounded-lg dark:bg-black focus:outline-none"
         >
           <ChannelTabs
+            v-show="smAndDown"
             class="mb-2 w-full border-b border-gray-200 bg-white px-3 dark:border-gray-600 dark:bg-gray-800"
             :vertical="false"
             :show-counts="true"
