bcec46c24a1f90a34273214fac4b577731c2b971
Catherine Luse <gennitdev@gmail.com>
2024-12-20 16:12:57 -0700

    Try to apply CSS the same in dev and prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 const svgToDataUri = require("mini-svg-data-uri");
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+    './app.vue'
+  ],
   darkMode: "class",
   important: true,
   theme: {
