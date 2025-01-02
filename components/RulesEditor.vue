134d3f9560dd11479d5ec4a0a8796d1e3e0fdc0d
Catherine Luse <gennitdev@gmail.com>
2025-01-02 04:54:32 -0700

    Create separate component for rules editor

---
+++ components/RulesEditor.vue
@@ -0,0 +1,83 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  formValues: {
+    rules: {
+      summary: string;
+      detail: string;
+    }[];
+  };
+  updateFormValues: (value: any) => void;
+}>();
+
+const emit = defineEmits(["updateFormValues"]);
+
+type RuleInput = {
+  summary: string;
+  detail: string;
+};
+
+const updateRule = (
+  index: number,
+  field: "summary" | "detail",
+  value: string
+) => {
+  const updatedRules: RuleInput[] = [...(props.formValues?.rules || [])];
+  updatedRules[index][field] = value;
+  emit("updateFormValues", { rules: updatedRules });
+};
+
+const addNewRule = (event: Event) => {
+  event.preventDefault();
+  const newRule = { summary: "", detail: "" };
+  const updatedRules = [...(props.formValues?.rules || []), newRule];
+  emit("updateFormValues", { rules: updatedRules });
+};
+
+const deleteRule = (index: number) => {
+  const updatedRules = [...(props.formValues?.rules || [])];
+  updatedRules.splice(index, 1);
+  emit("updateFormValues", { rules: updatedRules });
+};
+</script>
+<template>
+  <div class="divide-y divide-gray-500">
+    <div
+      v-for="(rule, index) in formValues.rules"
+      :key="index"
+      class="mb-4 flex flex-col gap-2"
+    >
+      <div class="flex justify-between">
+        <span class="font-bold mt-3 dark:text-white">Rule {{ index + 1 }}</span>
+        <button
+          class="mt-2 rounded border border-blue-500 px-2 py-1 text-blue-500 flex items-center gap-1"
+          @click="deleteRule(index)"
+        >
+          <XmarkIcon class="h-4" />
+          Delete Rule
+        </button>
+      </div>
+      <TextInput
+        :test-id="'rule-short-name-input-' + index"
+        :value="rule.summary"
+        :placeholder="'Rule short name'"
+        :full-width="true"
+        @update="updateRule(index, 'summary', $event)"
+      />
+      <TextEditor
+        :test-id="'rule-detail-input-' + index"
+        :initial-value="rule.detail || ''"
+        :placeholder="'Rule details'"
+        :rows="4"
+        :disable-auto-focus="true"
+        :allow-image-upload="false"
+        @update="updateRule(index, 'detail', $event)"
+      />
+    </div>
+  </div>
+  <button
+    class="mt-2 rounded border border-blue-500 px-2 py-1 text-blue-500"
+    @click="addNewRule"
+  >
+    + Add New Rule
+  </button>
+</template>
--- components/channel/form/CreateEditChannelFields.vue
+++ components/channel/form/CreateEditChannelFields.vue
@@ -140,30 +140,7 @@ const handleImageChange = async (input: FileChangeInput) => {
   }
 };
 
-type RuleInput = {
-  summary: string;
-  detail: string;
-};
 
-// Methods for handling rules
-const updateRule = (index: number, field: 'summary' | 'detail', value: string) => {
-  const updatedRules: RuleInput[] = [...(props.formValues?.rules || [])];
-  updatedRules[index][field] = value;
-  emit("updateFormValues", { rules: updatedRules });
-};
-
-const addNewRule = (event: Event) => {
-  event.preventDefault();
-  const newRule = { summary: "", detail: "" };
-  const updatedRules = [...(props.formValues?.rules || []), newRule];
-  emit("updateFormValues", { rules: updatedRules });
-};
-
-const deleteRule = (index: number) => {
-  const updatedRules = [...(props.formValues?.rules || [])];
-  updatedRules.splice(index, 1);
-  emit("updateFormValues", { rules: updatedRules });
-};
 
 const CHANNEL_ALREADY_EXISTS_ERROR = "Constraint validation failed";
 </script>
@@ -292,8 +269,6 @@ const CHANNEL_ALREADY_EXISTS_ERROR = "Constraint validation failed";
               />
             </template>
           </FormRow>
-
-          <!-- Forum Banner Upload -->
           <FormRow section-title="Forum Banner">
             <template #content>
               <img
@@ -311,50 +286,12 @@ const CHANNEL_ALREADY_EXISTS_ERROR = "Constraint validation failed";
               />
             </template>
           </FormRow>
-
-          <!-- Forum Rules -->
           <FormRow section-title="Forum Rules">
             <template #content>
-              <div class="divide-y divide-gray-500">
-                <div
-                  v-for="(rule, index) in formValues.rules"
-                  :key="index"
-                  class="mb-4 flex flex-col gap-2"
-                >
-                  <div class="flex justify-between">
-                    <span class="font-bold mt-3">Rule {{ index + 1 }}</span>
-                    <button
-                      class="mt-2 rounded border border-blue-500 px-2 py-1 text-blue-500 flex items-center gap-1"
-                      @click="deleteRule(index)"
-                    >
-                      <XmarkIcon class="h-4" />
-                      Delete Rule
-                    </button>
-                  </div>
-                  <TextInput
-                    :test-id="'rule-short-name-input-' + index"
-                    :value="rule.summary"
-                    :placeholder="'Rule short name'"
-                    :full-width="true"
-                    @update="updateRule(index, 'summary', $event)"
-                  />
-                  <TextEditor
-                    :test-id="'rule-detail-input-' + index"
-                    :initial-value="rule.detail || ''"
-                    :placeholder="'Rule details'"
-                    :rows="4"
-                    :disable-auto-focus="true"
-                    :allow-image-upload="false"
-                    @update="updateRule(index, 'detail', $event)"
-                  />
-                </div>
-              </div>
-              <button
-                class="mt-2 rounded border border-blue-500 px-2 py-1 text-blue-500"
-                @click="addNewRule"
-              >
-                + Add New Rule
-              </button>
+              <RulesEditor 
+                :form-values="formValues"
+                @update-form-values="$emit('updateFormValues', $event)"
+              />
             </template>
           </FormRow>
         </div>
