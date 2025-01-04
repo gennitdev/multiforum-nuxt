5f5a4566a8fb141bb61310e74fc8e9e590c40589
Catherine Luse <gennitdev@gmail.com>
2025-01-04 13:56:29 -0700

    Improve carousel styling

--- components/discussion/detail/DiscussionAlbum.vue
+++ components/discussion/detail/DiscussionAlbum.vue
@@ -49,32 +49,35 @@ const lightGalleryLicenseKey = config.lightgalleryLicenseKey;
       </span>
     </a>
   </lightgallery>
-  <lightgallery
-    v-else
-    :settings="{
-      speed: 500,
-      plugins: plugins,
-      licenseKey: lightGalleryLicenseKey,
-    }"
-    class="flex overflow-x-auto snap-x snap-mandatory space-x-4 pb-4 dark:text-white"
-  >
-    <a
-      v-for="image in album.Images"
-      :key="image.id"
-      :href="image.url || ''"
-      class="flex-none w-3/4 first:pl-4 last:pr-4 snap-center"
+
+    <lightgallery
+      v-else
+      :settings="{
+        speed: 500,
+        plugins: plugins,
+        licenseKey: lightGalleryLicenseKey,
+      }"
+      class="flex overflow-x-auto snap-x snap-mandatory space-x-4 pb-4 dark:text-white"
     >
-      <img
-        v-if="image"
-        :src="image.url || ''"
-        :alt="image.alt || ''"
-        class="w-full h-48 object-cover rounded-lg shadow-sm"
+      <a 
+        v-for="image in album.Images" 
+        :key="image.id" 
+        :href="image.url || ''"
+        class="flex-none w-3/4 first:pl-4 last:pr-4 snap-center flex flex-col"
       >
-      <span class="text-center block mt-2">
-        {{ image.alt }}
-      </span>
-    </a>
-  </lightgallery>
+        <div class="relative w-full pt-[56.25%]">
+          <img 
+            v-if="image" 
+            :src="image.url || ''" 
+            :alt="image.alt || ''" 
+            class="absolute inset-0 w-full h-full object-contain bg-gray-100 dark:bg-gray-800 rounded-lg shadow-sm"
+          >
+        </div>
+        <span class="text-center mt-2">
+          {{ image.alt }}
+        </span>
+      </a>
+    </lightgallery>
 </template>
 <style scoped>
 img {
