a984ccbac1819a2a35d1855ace8839b890f86f5b
Catherine Luse <gennitdev@gmail.com>
2025-01-06 17:44:36 -0700

    Carousel images are more functional on mobile

--- components/discussion/detail/DiscussionAlbum.vue
+++ components/discussion/detail/DiscussionAlbum.vue
@@ -8,10 +8,11 @@ import { config } from "@/config";
 import "lightgallery/css/lightgallery.css";
 import "lightgallery/css/lg-thumbnail.css";
 import "lightgallery/css/lg-zoom.css";
-
+import LeftArrowIcon from "@/components/icons/LeftArrowIcon.vue";
+import RightArrowIcon from "@/components/icons/RightArrowIcon.vue";
 import type { Album } from "@/__generated__/graphql";
 
-defineProps({
+const props = defineProps({
   album: {
     type: Object as PropType<Album>,
     required: true,
@@ -25,10 +26,30 @@ defineProps({
 const plugins = ref([lgThumbnail, lgZoom]);
 
 const lightGalleryLicenseKey = config.lightgalleryLicenseKey;
+const activeIndex = ref(0);
+const goLeft = () => {
+  // if already at the beginning, go to the end
+  if (activeIndex.value === 0) {
+    activeIndex.value = props.album.Images.length - 1;
+  } else {
+    activeIndex.value--;
+  }
+};
+const goRight = () => {
+  // if already at the end, go to the beginning
+  if (activeIndex.value === props.album.Images.length - 1) {
+    activeIndex.value = 0;
+  } else {
+    activeIndex.value++;
+  }
+};
 </script>
 
 <template>
-  <div class="overflow-x-auto">
+  <div class="overflow-x-auto border">
+    <span class="p-1">{{
+      `${activeIndex + 1} of ${album.Images.length}`
+    }}</span>
     <lightgallery
       v-if="!carouselFormat"
       :settings="{
@@ -51,36 +72,63 @@ const lightGalleryLicenseKey = config.lightgalleryLicenseKey;
       </a>
     </lightgallery>
 
-    <lightgallery
+    <div
       v-else
-      :settings="{
-        speed: 500,
-        plugins: plugins,
-        licenseKey: lightGalleryLicenseKey,
-      }"
-      class=" p-4 flex rounded gap-x-2 dark:text-white max-h-96 max-w-96"
+      class="flex items-center justify-center gap-2"
     >
-      <a
-        v-for="image in album.Images"
-        :key="image.id"
-        :href="image.url || ''"
-        class=" flex flex-shrink-0 w-auto"
+      <button
+        type="button"
+        class="h-36 hover:bg-gray-500 flex items-center justify-center px-2"
+        @click="goLeft"
       >
-        <div class="max-h-96 max-w-96">
-          <img
-            v-if="image"
-            :src="image.url || ''"
-            :alt="image.alt || ''"
-            class="shadow-sm"
-          >
-          <span class="text-center">
-            {{ image.alt }}
-          </span>
-        </div>
-      </a>
-    </lightgallery>
+        <LeftArrowIcon class="h-4 w-4" />
+      </button>
+      <lightgallery
+        :settings="{
+          speed: 500,
+          plugins: plugins,
+          licenseKey: lightGalleryLicenseKey,
+        }"
+        class="m-4 flex rounded dark:text-white max-h-96 max-w-96"
+      >
+        <a
+          v-for="(image, idx) in album.Images"
+          :key="image.id"
+          :href="image.url || ''"
+          class="flex flex-shrink-0 w-auto"
+        >
+          <div class="max-h-96 max-w-96">
+            <img
+              v-if="image"
+              :src="image.url || ''"
+              :alt="image.alt || ''"
+              class="shadow-sm"
+              :class="{
+                hidden: idx !== activeIndex,
+              }"
+            >
+            <span
+              class="text-center"
+              :class="{
+                hidden: idx !== activeIndex,
+              }"
+            >
+              {{ image.alt }}
+            </span>
+          </div>
+        </a>
+      </lightgallery>
+      <button
+        class="h-36 hover:bg-gray-500 flex items-center justify-center px-2"
+        type="button"
+        @click="goRight"
+      >
+        <RightArrowIcon class="h-4 w-4" />
+      </button>
+    </div>
   </div>
 </template>
+
 
 <style scoped>
 img {
