cd82924ba9a94cbf32e41fa44494c7e9738f08eb
Catherine Luse <gennitdev@gmail.com>
2025-01-12 21:05:37 -0700

    Comment author age is correct

--- components/comments/CommentHeader.vue
+++ components/comments/CommentHeader.vue
@@ -1,142 +1,100 @@
-<script lang="ts">
-import type { PropType } from "vue";
-import { defineComponent, computed } from "vue";
-import type { Comment, User, ModerationProfile } from "@/__generated__/graphql";
-import UsernameWithTooltip from "../UsernameWithTooltip.vue";
-// @ts-ignore
-import clickOutside from "vue-click-outside";
-import { relativeTime } from "@/utils";
+<script setup lang="ts">
+import { computed } from 'vue'
+import type { PropType } from 'vue'
+import type { Comment, User, ModerationProfile } from '@/__generated__/graphql'
+import UsernameWithTooltip from '../UsernameWithTooltip.vue'
+import { relativeTime } from '@/utils'
 
-export default defineComponent({
-  components: {
-    UsernameWithTooltip,
+// Props definition using defineProps
+const props = defineProps({
+  commentData: {
+    type: Object as PropType<Comment>,
+    required: true,
   },
-  directives: {
-    clickOutside,
+  isHighlighted: {
+    type: Boolean,
+    default: false,
   },
-  props: {
-    commentData: {
-      type: Object as PropType<Comment>,
-      required: true,
-    },
-    isHighlighted: {
-      type: Boolean,
-      default: false,
-    },
-    parentCommentId: {
-      type: String,
-      default: "",
-    },
-    showContextLink: {
-      type: Boolean,
-      default: true,
-    },
-    showChannel: {
-      type: Boolean,
-      default: false,
-    },
-    originalPoster: {
-      type: String,
-      default: "",
-    },
-    label: {
-      type: String,
-      default: "",
-    },
+  parentCommentId: {
+    type: String,
+    default: '',
   },
-  setup(props) {
-    const isAdmin = computed(() => {
-      const author: User | ModerationProfile | undefined | null =
-        props.commentData.CommentAuthor;
-      if (!author) {
-        return false;
-      }
-      if (author.__typename === "User") {
-        const serverRoles = author.ServerRoles;
-        if (!serverRoles) {
-          return false;
-        }
-        if (serverRoles.length === 0) {
-          return false;
-        }
-        const serverRole = serverRoles[0];
-        if (serverRole.showAdminTag) {
-          return true;
-        }
-      }
+  showContextLink: {
+    type: Boolean,
+    default: true,
+  },
+  showChannel: {
+    type: Boolean,
+    default: false,
+  },
+  originalPoster: {
+    type: String,
+    default: '',
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+})
+const isAdmin = computed(() => {
+  const author: User | ModerationProfile | undefined | null = props.commentData.CommentAuthor
+  if (!author) {
+    return false
+  }
+  if (author.__typename === 'User') {
+    const serverRoles = author.ServerRoles
+    if (!serverRoles || serverRoles.length === 0) {
+      return false
+    }
+    return serverRoles[0].showAdminTag
+  }
+  return false
+})
 
-      return false;
-    });
+const isMod = computed(() => {
+  const author: User | ModerationProfile | undefined | null = props.commentData.CommentAuthor
+  if (!author) {
+    return false
+  }
+  if (author.__typename === 'User') {
+    const channelRoles = author.ChannelRoles
+    if (!channelRoles || channelRoles.length === 0) {
+      return false
+    }
+    return channelRoles[0].showModTag
+  }
+  return false
+})
 
-    const isMod = computed(() => {
-      const author: User | ModerationProfile | undefined | null =
-        props.commentData.CommentAuthor;
-      if (!author) {
-        return false;
-      }
-      if (author.__typename === "User") {
-        const channelRoles = author.ChannelRoles;
-        if (!channelRoles) {
-          return false;
-        }
-        if (channelRoles.length === 0) {
-          return false;
-        }
-        const channelRole = channelRoles[0];
-        if (channelRole.showModTag) {
-          return true;
-        }
-        return false;
-      }
-      return false;
-    });
-    return {
-      isAdmin,
-      isMod,
-      relativeTime,
-    };
-  },
-  computed: {
-    commentAuthorUsername() {
-      return this.commentData.CommentAuthor?.username;
-    },
-    commentAuthorProfilePic() {
-      return this.commentData.CommentAuthor?.profilePicURL;
-    },
-    commentAuthorDisplayName() {
-      return this.commentData.CommentAuthor?.displayName;
-    },
-    commentKarma() {
-      return this.commentData.CommentAuthor?.commentKarma ?? 0;
-    },
-    discussionKarma() {
-      return this.commentData.CommentAuthor?.discussionKarma ?? 0;
-    },
-    createdAt() {
-      return this.commentData.createdAt;
-    },
-    createdAtFormatted() {
-      if (!this.commentData.createdAt) {
-        return "";
-      }
-      return `posted ${this.relativeTime(this.commentData.createdAt)}${
-        this.showChannel
-          ? " in c/" +
-            (this.commentData.DiscussionChannel?.channelUniqueName ||
-              this.commentData.Event?.EventChannels[0]?.channelUniqueName ||
-                this.commentData.Channel?.uniqueName ||
-              "")
-          : ""
-      }`;
-    },
-    editedAtFormatted() {
-      if (!this.commentData.updatedAt) {
-        return "";
-      }
-      return `Edited ${this.relativeTime(this.commentData.updatedAt)}`;
-    },
-  },
-});
+const commentAuthorUsername = computed(() => props.commentData.CommentAuthor?.username)
+const commentAuthorProfilePic = computed(() => props.commentData.CommentAuthor?.profilePicURL)
+const commentAuthorDisplayName = computed(() => props.commentData.CommentAuthor?.displayName)
+const commentKarma = computed(() => props.commentData.CommentAuthor?.commentKarma ?? 0)
+const discussionKarma = computed(() => props.commentData.CommentAuthor?.discussionKarma ?? 0)
+const commentAuthorAge = computed(() => props.commentData.CommentAuthor?.createdAt)
+const createdAt = computed(() => props.commentData.createdAt)
+
+const createdAtFormatted = computed(() => {
+  if (!props.commentData.createdAt) {
+    return ''
+  }
+  return `posted ${relativeTime(props.commentData.createdAt)}${
+    props.showChannel
+      ? ' in c/' +
+        (props.commentData.DiscussionChannel?.channelUniqueName ||
+          props.commentData.Event?.EventChannels[0]?.channelUniqueName ||
+          props.commentData.Channel?.uniqueName ||
+          '')
+      : ''
+  }`
+})
+
+const editedAtFormatted = computed(() => {
+  if (!props.commentData.updatedAt) {
+    return ''
+  }
+  return `Edited ${relativeTime(props.commentData.updatedAt)}`
+})
 </script>
 
 <template>
@@ -157,15 +115,13 @@ export default defineComponent({
     />
 
     <div
-      :class="!commentAuthorUsername ? '-ml-5' : '-ml-4 border-l '"
+      :class="!commentAuthorUsername ? '-ml-5' : '-ml-4 border-l'"
       class="flex-grow border-gray-300 pl-4 dark:border-gray-600"
     >
       <div
-        v-if="
-          showContextLink && parentCommentId && commentData.DiscussionChannel
-        "
+        v-if="showContextLink && parentCommentId && commentData.DiscussionChannel"
       >
-        <nuxt-link
+        <NuxtLink
           class="px-2 text-xs underline"
           :to="{
             name: 'forums-forumId-discussions-discussionId-comments-commentId',
@@ -177,13 +133,11 @@ export default defineComponent({
           }"
         >
           View Context
-        </nuxt-link>
+        </NuxtLink>
       </div>
       <div class="mt-2 flex flex-wrap items-center">
-        <div
-          class="ml-1 flex flex-wrap items-center space-x-2 text-xs dark:text-gray-300"
-        >
-          <nuxt-link
+        <div class="ml-1 flex flex-wrap items-center space-x-2 text-xs dark:text-gray-300">
+          <NuxtLink
             v-if="commentAuthorUsername"
             class="mx-1 font-bold hover:underline dark:text-gray-200"
             :to="{
@@ -196,15 +150,15 @@ export default defineComponent({
               :username="commentAuthorUsername"
               :src="commentAuthorProfilePic || ''"
               :display-name="commentAuthorDisplayName || ''"
-              :comment-karma="commentKarma ?? 0"
-              :discussion-karma="discussionKarma ?? 0"
-              :account-created="createdAt"
+              :comment-karma="commentKarma"
+              :discussion-karma="discussionKarma"
+              :account-created="commentAuthorAge"
               :is-admin="isAdmin"
               :is-mod="isMod"
               :is-original-poster="commentAuthorUsername === originalPoster"
             />
-          </nuxt-link>
-          <nuxt-link
+          </NuxtLink>
+          <NuxtLink
             v-else-if="commentData.CommentAuthor?.displayName"
             class="mx-1 font-bold hover:underline dark:text-gray-200"
             :to="{
@@ -213,23 +167,22 @@ export default defineComponent({
             }"
           >
             {{ commentData.CommentAuthor.displayName }}
-          </nuxt-link>
+          </NuxtLink>
           <span v-else class="flex items-center font-bold">
-            <div
-              class="mr-2 h-8 w-8 rounded-full border dark:border-gray-500"
-            />
+            <div class="mr-2 h-8 w-8 rounded-full border dark:border-gray-500" />
             [Deleted]
           </span>
           <span class="mx-2">&middot;</span>
           <span>{{ createdAtFormatted }}</span>
-          <span v-if="commentData.updatedAt" class="mx-2"> &middot; </span>
+          <span v-if="commentData.updatedAt" class="mx-2">&middot;</span>
           <span>{{ editedAtFormatted }}</span>
           <span
             v-if="isHighlighted"
             class="rounded-lg bg-blue-500 px-2 py-1 text-black"
-            >Permalinked
+          >
+            Permalinked
           </span>
-          <span 
+          <span
             v-if="label"
             class="rounded-lg border border-blue-500 px-2 py-1 text-blue-600 dark:text-blue-400 bg-blue-100 dark:bg-blue-900"
           >
