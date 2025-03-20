b957f363aca6abe86a4688436aac710325b8ee45
Catherine Luse <gennitdev@gmail.com>
2025-03-20 14:01:12 -0700

    Make privacy related console warnings go away

--- plugins/auth0.client.ts
+++ plugins/auth0.client.ts
@@ -1,10 +1,11 @@
 // plugins/auth0.client.ts
-import { createAuth0 } from "@auth0/auth0-vue";
+import { createAuth0, type Auth0VueClientOptions } from "@auth0/auth0-vue";
+import { defineNuxtPlugin, useRuntimeConfig } from "nuxt/app";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig().public;
 
-  const createAuth0options = {
+  const createAuth0options: Auth0VueClientOptions = {
     domain: config.auth0Domain,
     clientId: config.auth0ClientId,
     authorizationParams: {
@@ -12,6 +13,8 @@ export default defineNuxtPlugin((nuxtApp) => {
       redirect_uri: config.auth0CallbackUrl,
       prompt: "select_account",
     },
+    cacheLocation: "localstorage" as const,
+    useRefreshTokens: true,
   };
 
   const auth0 = createAuth0(createAuth0options);
