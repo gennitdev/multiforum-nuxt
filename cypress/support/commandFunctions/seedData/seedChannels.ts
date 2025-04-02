39818a356d8feb7b2c41d28b6cfe8815bc7dc9d9
Catherine Luse <gennitdev@gmail.com>
2025-04-01 20:59:32 -0700

    Add mods while seeding channels

--- cypress/support/commandFunctions/seedData/seedChannels.ts
+++ cypress/support/commandFunctions/seedData/seedChannels.ts
@@ -3,32 +3,47 @@ import type { ChannelCreateInput } from "../../../../__generated__/graphql";
 type BaseChannel = {
   uniqueName: string;
   admins: string[];
+  mods: string[];
 };
 
 const baseChannels: BaseChannel[] = [
   {
     uniqueName: "cats",
     admins: ["cluse"],
+    mods: ["testModProfile2"]
   },
   {
     uniqueName: "phx_music",
     admins: ["alice"],
+    mods: ["testModProfile1"]
   },
   {
     uniqueName: "phx_concerts",
     admins: ["alice"],
+    mods: ["testModProfile1"]
   },
 ];
 
 const channels: ChannelCreateInput[] = baseChannels.map(
-  ({ uniqueName, admins }) => ({
+  ({ uniqueName, admins, mods }) => ({
     uniqueName,
     Admins: {
       connect: admins.map((admin) => ({
+          where: {
+            node: {
+              username: admin,
+          }
+        },
+      })),
+    },
+    Moderators: {
+      connect: mods.map((mod) => ({
         where: {
           node: {
-            username: admin,
-          },
+            ModerationProfile: {
+              displayName: mod,
+            },
+          }
         },
       })),
     },
