ccc0223b0578058db19ee00e22998a1c3f121fa9
Catherine Luse <gennitdev@gmail.com>
2025-04-18 06:57:12 -0700

    Tab titles are correct

--- components/InfoBanner.vue
+++ components/InfoBanner.vue
@@ -12,7 +12,7 @@ defineProps({
 
 <template>
   <div
-    class="text-wrap w-full text-sm inline-flex border border-blue-600 text-blue-700 bg-white p-2 space-x-1 my-1 rounded pl-4 dark:bg-black dark:text-blue-500 gap-1"
+    class="text-wrap w-full text-sm inline-flex border border-blue-600 text-blue-900 bg-white p-2 space-x-1 my-1 rounded pl-4 dark:bg-black dark:text-blue-500 gap-1"
   >
     <slot>
       <InfoIcon class="h-5 w-5 mt-2 flex-grow-0" />
