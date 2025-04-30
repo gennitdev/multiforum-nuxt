fdc8fd2f2bb2465eb917dd7795d2edd76c551cab
Catherine Luse <gennitdev@gmail.com>
2025-04-29 22:42:25 -0700

    Add #fallback to username with tooltip

--- components/UsernameWithTooltip.vue
+++ components/UsernameWithTooltip.vue
@@ -59,91 +59,131 @@ export default defineComponent({
 </script>
 <template>
   <client-only>
-  <v-tooltip
-    location="bottom"
-    content-class="custom-tooltip"
-  >
-    <template #activator="{ props }">
-      <button v-bind="props">
-        <slot>
-          <div class="flex flex-row items-center gap-1">
-            <nuxt-link
-              :to="{
-                name: 'u-username',
-                params: { username },
-              }"
-              class="flex flex-row items-center gap-1 hover:underline"
-            >
+    <v-tooltip
+      location="bottom"
+      content-class="custom-tooltip"
+    >
+      <template #activator="{ props }">
+        <button v-bind="props">
+          <slot>
+            <div class="flex flex-row items-center gap-1">
+              <nuxt-link
+                :to="{
+                  name: 'u-username',
+                  params: { username },
+                }"
+                class="flex flex-row items-center gap-1 hover:underline"
+              >
+                <span
+                  v-if="!displayName"
+                  class="font-bold"
+                >{{ username }}</span>
+                <span
+                  v-if="displayName"
+                  class="font-bold"
+                >{{
+                  displayName
+                }}</span>
+                <span
+                  v-if="displayName"
+                  class="text-gray-500 dark:text-gray-300"
+                >{{ `(u/${username})` }}</span>
+              </nuxt-link>
               <span
-                v-if="!displayName"
-                class="font-bold"
-              >{{ username }}</span>
+                v-if="isAdmin"
+                class="rounded-md border border-gray-500 dark:border-gray-300 px-1 py-0 text-xs text-gray-500 dark:text-gray-300"
+              >Admin</span>
               <span
-                v-if="displayName"
-                class="font-bold"
-              >{{
-                displayName
-              }}</span>
+                v-else-if="isMod"
+                class="rounded-md border border-blue-500 dark:border-gray-300 px-1 py-0 text-xs text-gray-500 dark:text-gray-300"
+              >
+                Mod
+              </span>
               <span
-                v-if="displayName"
-                class="text-gray-500 dark:text-gray-300"
-              >{{ `(u/${username})` }}</span>
-            </nuxt-link>
-            <span
-              v-if="isAdmin"
-              class="rounded-md border border-gray-500 dark:border-gray-300 px-1 py-0 text-xs text-gray-500 dark:text-gray-300"
-            >Admin</span>
-            <span
-              v-else-if="isMod"
-              class="rounded-md border border-blue-500 dark:border-gray-300 px-1 py-0 text-xs text-gray-500 dark:text-gray-300"
-            >
-              Mod
-            </span>
-            <span
-              v-if="isOriginalPoster"
-              class="rounded-md border border-gray-500 dark:border-gray-300 px-1 py-0 text-xs text-gray-500 dark:text-gray-300"
-            >OP</span>
+                v-if="isOriginalPoster"
+                class="rounded-md border border-gray-500 dark:border-gray-300 px-1 py-0 text-xs text-gray-500 dark:text-gray-300"
+              >OP</span>
+            </div>
+          </slot>
+        </button>
+      </template>
+      <template #default>
+        <div>
+          <div
+            v-if="!displayName"
+            class="text-md flex w-full flex-col"
+          >
+            <AvatarComponent
+              :text="username"
+              :src="src"
+              :is-medium="true"
+            />{{ username }}
           </div>
-        </slot>
-      </button>
-    </template>
-    <template #default>
-      <div>
-        <div
-          v-if="!displayName"
-          class="text-md flex w-full flex-col"
-        >
-          <AvatarComponent
-            :text="username"
-            :src="src"
-            :is-medium="true"
-          />{{ username }}
+          <div
+            v-if="displayName"
+            class="text-md flex w-full flex-col"
+          >
+            <AvatarComponent
+              :text="username"
+              :src="src"
+              :is-medium="true"
+            />
+            <p class="text-xs font-bold">
+              {{ displayName }}
+            </p>
+            <p class="text-xs text-gray-600 dark:text-white">
+              {{ `u/${username}` }}
+            </p>
+          </div>
+          <ul class="text-xs">
+            <li>
+              {{ `account created ${timeAgo(new Date(accountCreated))}` }}
+            </li>
+            <li>{{ `${commentKarma ?? 0} comment karma` }}</li>
+            <li>{{ `${discussionKarma ?? 0} discussion karma` }}</li>
+          </ul>
         </div>
-        <div
-          v-if="displayName"
-          class="text-md flex w-full flex-col"
+      </template>
+    </v-tooltip>
+    
+    <!-- SSR Fallback -->
+    <template #fallback>
+      <div class="flex flex-row items-center gap-1">
+        <nuxt-link
+          :to="{
+            name: 'u-username',
+            params: { username },
+          }"
+          class="flex flex-row items-center gap-1 hover:underline"
         >
-          <AvatarComponent
-            :text="username"
-            :src="src"
-            :is-medium="true"
-          />
-          <p class="text-xs font-bold">
-            {{ displayName }}
-          </p>
-          <p class="text-xs text-gray-600 dark:text-white">
-            {{ `u/${username}` }}
-          </p>
-        </div>
-        <ul class="text-xs">
-          <li>
-            {{ `account created ${timeAgo(new Date(accountCreated))}` }}
-          </li>
-          <li>{{ `${commentKarma ?? 0} comment karma` }}</li>
-          <li>{{ `${discussionKarma ?? 0} discussion karma` }}</li>
-        </ul>
+          <span
+            v-if="!displayName"
+            class="font-bold"
+          >{{ username }}</span>
+          <span
+            v-if="displayName"
+            class="font-bold"
+          >{{ displayName }}</span>
+          <span
+            v-if="displayName"
+            class="text-gray-500 dark:text-gray-300"
+          >{{ `(u/${username})` }}</span>
+        </nuxt-link>
+        <span
+          v-if="isAdmin"
+          class="rounded-md border border-gray-500 dark:border-gray-300 px-1 py-0 text-xs text-gray-500 dark:text-gray-300"
+        >Admin</span>
+        <span
+          v-else-if="isMod"
+          class="rounded-md border border-blue-500 dark:border-gray-300 px-1 py-0 text-xs text-gray-500 dark:text-gray-300"
+        >
+          Mod
+        </span>
+        <span
+          v-if="isOriginalPoster"
+          class="rounded-md border border-gray-500 dark:border-gray-300 px-1 py-0 text-xs text-gray-500 dark:text-gray-300"
+        >OP</span>
       </div>
     </template>
-  </v-tooltip>
-</client-only>
+  </client-only>
 </template>
