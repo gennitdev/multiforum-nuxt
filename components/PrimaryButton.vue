93ec49c658f9119d34c43c26f5957e514ca9bff8
Catherine Luse <gennitdev@gmail.com>
2025-05-01 21:41:16 -0700

    Title changes size based on screen width

--- components/PrimaryButton.vue
+++ components/PrimaryButton.vue
@@ -24,7 +24,7 @@ defineProps({
     :class="[
       disabled
         ? 'cursor-default bg-gray-200 text-gray-300 dark:bg-gray-800 dark:text-gray-300'
-        : ' bg-blue-600 text-white hover:bg-gray-200 dark:border dark:border-blue-600 dark:bg-blue-500 dark:text-black dark:hover:bg-blue-400',
+        : ' bg-blue-600 text-white hover:bg-blue-700 dark:border dark:border-blue-600 dark:bg-blue-500 dark:text-black dark:hover:bg-blue-400',
       '',
     ]"
     class="max-height-4 inline-flex items-center whitespace-nowrap rounded-md px-4 py-2 text-sm font-medium focus:outline-none focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"
