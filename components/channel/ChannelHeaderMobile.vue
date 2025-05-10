5d41c4464fe7f55a5008696a3697cd32dd2fb4ee
Catherine Luse <gennitdev@gmail.com>
2025-05-09 18:12:31 -0700

    Adjust spacing for channel header on mobile

--- components/channel/ChannelHeaderMobile.vue
+++ components/channel/ChannelHeaderMobile.vue
@@ -15,7 +15,7 @@ defineProps({
   <div class="flex items-center justify-between">
     <div class="flex flex-row items-center justify-center gap-4 dark:bg-black mb-2">
       <AvatarComponent
-        class="flex h-14 w-14 justify-center align-items shadow-sm pt-2 ml-2"
+        class="flex h-14 w-14 justify-center align-items shadow-sm pt-2 ml-2 my-2"
         :text="channelId"
         :src="channel?.channelIconURL ?? ''"
         :is-square="false"
@@ -23,19 +23,19 @@ defineProps({
       <div v-if="channel.displayName && channel.uniqueName" class="mt-4">
         <h1
           v-if="channel.displayName"
-          class="flex border-gray-700 mt-2 text-2xl leading-6 text-black dark:text-white text-outline"
+          class="flex border-gray-700 mt-4 text-2xl leading-6 text-black dark:text-white text-outline"
         >
           {{ channel.displayName }}
         </h1>
         <h2
-          class="text-sm font-mono leading-6 text-black dark:text-gray-300"
+          class="text-sm font-mono leading-6 text-black dark:text-gray-300 mt-4"
         >
           {{ `${channel.uniqueName}` }}
         </h2>
       </div>
       <h1
         v-else
-        class="flex mb-0 mt-3 border-gray-700 text-2xl leading-6 text-black dark:text-white"
+        class="flex mb-0 mt-6 border-gray-700 text-2xl leading-6 text-black dark:text-white"
       >
         {{ channelId }}
       </h1>
