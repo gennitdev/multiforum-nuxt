6ae56ae1442606fcbd81c05a000fbed744c99418
Catherine Luse <gennitdev@gmail.com>
2025-05-24 02:43:04 -0700

    All discussions in test data are created with hasDownload: false

--- tests/cypress/support/commandFunctions/seedData/seedDiscussions.ts
+++ tests/cypress/support/commandFunctions/seedData/seedDiscussions.ts
@@ -42,6 +42,7 @@ export const discussions: DiscussionCreateInputWithChannels[] =
     discussionCreateInput: {
       title,
       ...(body && { body }),
+      hasDownload: false,
       Author: {
         connect: {
           where: {
