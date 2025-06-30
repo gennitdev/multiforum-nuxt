0bfee449eef4e59891c016169ed7511b96e7f6ae
Catherine Luse <gennitdev@gmail.com>
2025-06-29 17:14:36 -0700

    Fix console error related to popper

--- components/FilterChip.vue
+++ components/FilterChip.vue
@@ -51,19 +51,21 @@ export default defineComponent({
         :close-on-content-click="false"
         location="bottom"
       >
-        <button
-          :data-testid="dataTestid"
-          :class="[highlighted ? 'border-orange-500 ring-1 ring-orange-500' : '']"
-          class="font-small flex align-items whitespace-nowrap rounded-md bg-white px-3 py-2 text-xs text-gray-700 border hover:bg-gray-200 focus:border-orange-500 focus:ring-1 focus:ring-orange-500 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-200 dark:hover:bg-gray-600"
-          @click="handleClick"
-        >
-          <slot name="icon" />
-          {{ label }}
-          <ChevronDownIcon
-            class="-mr-1 ml-1 mt-0.5 h-3 w-3"
-            aria-hidden="true"
-          />
-        </button>
+        <template #default>
+          <button
+            :data-testid="dataTestid"
+            :class="[highlighted ? 'border-orange-500 ring-1 ring-orange-500' : '']"
+            class="font-small flex align-items whitespace-nowrap rounded-md bg-white px-3 py-2 text-xs text-gray-700 border hover:bg-gray-200 focus:border-orange-500 focus:ring-1 focus:ring-orange-500 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-200 dark:hover:bg-gray-600"
+            @click="handleClick"
+          >
+            <slot name="icon" />
+            {{ label }}
+            <ChevronDownIcon
+              class="-mr-1 ml-1 mt-0.5 h-3 w-3"
+              aria-hidden="true"
+            />
+          </button>
+        </template>
         <template #content>
           <div
             class="rounded-md border bg-white dark:bg-gray-700"
