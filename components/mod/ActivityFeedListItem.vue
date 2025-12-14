e54c0ccf57828727e2767585b71abff2e1e77daf
Catherine Luse <gennitdev@gmail.com>
2025-12-14 06:24:17 -0700

    Add in-place editing for issue comments

--- components/mod/ActivityFeedListItem.vue
+++ components/mod/ActivityFeedListItem.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
 import MarkdownPreview from '../MarkdownPreview.vue';
-import type { PropType } from 'vue';
+import TextEditor from '@/components/TextEditor.vue';
+import GenericButton from '@/components/GenericButton.vue';
+import SaveButton from '@/components/SaveButton.vue';
+import ErrorBanner from '@/components/ErrorBanner.vue';
+import { ref, computed, watch, type PropType } from 'vue';
 import { timeAgo } from '@/utils';
 import type { ModerationAction } from '@/__generated__/graphql';
 import { useRoute } from 'nuxt/app';
@@ -14,6 +18,9 @@ import ArrowPath from '../icons/ArrowPath.vue';
 import FlagIcon from '../icons/FlagIcon.vue';
 import XCircleIcon from '../icons/XCircleIcon.vue';
 import { ActionType } from '@/types/Comment';
+import { useMutation } from '@vue/apollo-composable';
+import { UPDATE_COMMENT } from '@/graphQLData/comment/mutations';
+import { modProfileNameVar, usernameVar } from '@/cache';
 
 const actionTypeToIcon = {
   [ActionType.Close]: XCircleIcon,
@@ -40,6 +47,79 @@ const props = defineProps({
 const commentIdInParams = useRoute().params.commentId as string;
 const isPermalinked =
   commentIdInParams && commentIdInParams === props.activityItem.Comment?.id;
+
+const isCommentAuthor = computed(() => {
+  const author = props.activityItem.Comment?.CommentAuthor;
+  if (!author) return false;
+
+  if ('username' in author) {
+    return !!usernameVar.value && author.username === usernameVar.value;
+  }
+
+  if ('displayName' in author) {
+    return !!modProfileNameVar.value && author.displayName === modProfileNameVar.value;
+  }
+
+  return false;
+});
+
+const isEditing = ref(false);
+const editedComment = ref(props.activityItem.Comment?.text || '');
+
+watch(
+  () => props.activityItem.Comment?.text,
+  (newText) => {
+    if (!isEditing.value) {
+      editedComment.value = newText || '';
+    }
+  },
+  { immediate: true }
+);
+
+const hasChanges = computed(() => {
+  return (editedComment.value || '') !== (props.activityItem.Comment?.text || '');
+});
+
+const {
+  mutate: updateComment,
+  loading: updateCommentLoading,
+  error: updateCommentError,
+  onDone: onUpdateDone,
+} = useMutation(UPDATE_COMMENT, () => ({
+  variables: {
+    commentWhere: { id: props.activityItem.Comment?.id },
+    updateCommentInput: { text: editedComment.value },
+  },
+}));
+
+onUpdateDone(() => {
+  isEditing.value = false;
+});
+
+const startEdit = () => {
+  if (!isCommentAuthor.value) return;
+  editedComment.value = props.activityItem.Comment?.text || '';
+  isEditing.value = true;
+};
+
+const cancelEdit = () => {
+  editedComment.value = props.activityItem.Comment?.text || '';
+  isEditing.value = false;
+};
+
+const saveEdit = async () => {
+  if (!props.activityItem.Comment?.id) return;
+  if (!editedComment.value.trim()) return;
+  if (!hasChanges.value) {
+    isEditing.value = false;
+    return;
+  }
+  try {
+    await updateComment();
+  } catch (error) {
+    console.error('Error updating comment', error);
+  }
+};
 </script>
 
 <template>
@@ -136,11 +216,52 @@ const isPermalinked =
           </div>
 
           <div class="border-l-2 border-gray-200 pl-2 dark:border-gray-500">
-            <MarkdownPreview
+            <div
               v-if="activityItem.Comment"
-              :text="activityItem.Comment.text || ''"
-              :word-limit="1000"
-              :disable-gallery="true"
+              class="mb-2 flex items-center justify-between gap-2"
+            >
+              <MarkdownPreview
+                v-if="!isEditing"
+                :text="activityItem.Comment.text || ''"
+                :word-limit="1000"
+                :disable-gallery="true"
+              />
+              <div v-else class="w-full space-y-2">
+                <TextEditor
+                  :key="`activity-comment-${activityItem.Comment?.id}`"
+                  :rows="4"
+                  :allow-image-upload="false"
+                  :initial-value="editedComment"
+                  :placeholder="'Edit your comment...'"
+                  @update="editedComment = $event"
+                />
+              </div>
+            </div>
+            <div
+              v-if="activityItem.Comment && isCommentAuthor"
+              class="flex flex-wrap items-center gap-2"
+            >
+              <GenericButton
+                v-if="!isEditing"
+                :text="'Edit'"
+                @click="startEdit"
+              />
+              <template v-else>
+                <GenericButton :text="'Cancel'" @click="cancelEdit" />
+                <SaveButton
+                  :label="'Save'"
+                  :disabled="
+                    updateCommentLoading || !editedComment.trim() || !hasChanges
+                  "
+                  :loading="updateCommentLoading"
+                  @click="saveEdit"
+                />
+              </template>
+            </div>
+            <ErrorBanner
+              v-if="updateCommentError"
+              class="mt-2"
+              :text="updateCommentError.message"
             />
           </div>
         </div>
